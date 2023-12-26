1d3b9b750b93944bca6d67219a3ba6cf108a46a3
Bohdana1958 <139041084+Bohdana1958@users.noreply.github.com>
2023-12-26 21:35:22 +0100

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,6 @@
-import './fetchAPI.js'
-import './localStorage.js'
-import './refs.js'
-import './renderFood.js'
-import './discount.js'
-
 import './fetchAPI.js';
 import './localStorage.js';
 import './pagination.js';
 import './modal_window.js';
 import './footer.js';
-
 import './discount.js';
