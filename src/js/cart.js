468d0cc45893e6d49ca386cdd1a81bd1702bfb8c
KateDro <106846150+KateDro@users.noreply.github.com>
2023-12-27 20:25:56 +0100

    upd

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,10 +1,11 @@
 function displayCart() {
-	let j = 0;
-	document.getElementById("card-counter").innerHTML = {< img class="basket-img" src = "../img/yellow-basket.png" width = "164px" height = "140px" alt = "Yellow shopping basket" >
+  let j = 0;
+  document.getElementById(
+    'card-counter'
+  ).innerHTML = `<img class="basket-img" src = "../img/yellow-basket.png" width = "164px" height = "140px" alt = "Yellow shopping basket" >
 		<h2 class="basket-title">
 			Your basket is <span class="accent-txt">empty...</span>
 		</h2>
 		<p class="basket-txt">Go to the main page to select your favorite products and add them to the cart.</p>
-}
-;
+`;
 }
