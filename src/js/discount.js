f2f3ad163c164f6da4baee1c06763556fb9ce339
Bohdana1958 <139041084+Bohdana1958@users.noreply.github.com>
2023-12-27 15:30:52 +0100

    disabled button

--- src/js/discount.js
+++ src/js/discount.js
@@ -1,93 +1,100 @@
-import { getAllProducts, getCategoriesProducts, getDiscountProducts,
-    getPopularProducts, getProductById, createNewOrder, sendSubscription } from "./fetchAPI"
-
+import { getAllProducts, getCategoriesProducts, getDiscountProducts, getPopularProducts, getProductById, createNewOrder, sendSubscription } from "./fetchAPI"
 import { openModal } from "./modal_window"
+import { addToStorageCart, removeFromStorageCart, isExistInCart } from "./localStorage"
 
+const card = document.querySelector('.card');
+// Ліміт карток на сторінці
+const cardsPerPage = 2;
 
-
-const card = document.querySelector('.card')
-
-
-
-function createMarkup(images, names) {
-  const res = images.map(({img, price,_id, desc}, index)=>
-  `<li class="card_item" data-id="${_id}">
-  <div class><svg class="discount-icon" width="60" height="60">
-  <use href="../img/icons.svg#icon-discount"></use>
-</svg></div>
-  <div class="card-content">
-  <img src="${img}" alt="${desc}" class="card-img" width="105" height="105">
-  </div>
-  <div class="title-box">
-    <h3 class="card-title">${names[index]}</h3>
-    <p class="card-price">$${price}</p>
-  </div>
-  <button class="basket">
-          <svg class="basket-icon" width="18" height="18">
-            <use href="../img/icons.svg#icon-cart"></use>
-          </svg>
-  </button>
-</li>`
-).join('');
-return res;
+function createMarkup(images, names,startIndex = 0) {
+  const endIndex = startIndex + cardsPerPage;
+  const slicedImages = images.slice(startIndex, endIndex);
   
+  const res = slicedImages.map(({ img, price, _id, desc }, index) =>
+    `<li class="card_item" data-id="${_id}">
+      <div class="discount-icon-container">
+        <svg class="discount-icon" width="60" height="60">
+          <use href="../img/icons.svg#icon-discount"></use>
+        </svg>
+      </div>
+      <div class="card-content">
+        <img src="${img}" alt="${desc}" class="card-img" width="105" height="105">
+      </div>
+      <div class="title-box">
+        <h3 class="card-title">${names[index]}</h3>
+        <p class="card-price">$${price}</p>
+      </div>
+      <button class="basket" data-id="${_id}">
+        <svg class="basket-icon" width="18" height="18">
+          <use href="../img/icons.svg#icon-cart"></use>
+        </svg>
+      </button>
+    </li>`
+  ).join('');
+  return res;
 };
 
-
-
-// Виклик модалки
-  card.addEventListener('click', (event)=>{const item = event.target.closest('.card_item')
-  const id = item.dataset.id
-  openModal(id)
-})
-
-
- async function addToBasket () {
-    const result = await getDiscountProducts();
-console.log(result);
-    const shortNames = result.reduce((acc, product)=> {
-      if (product.name.length > 10){
-        acc.push(product.name.slice(0, 10) + "...");
-      }else{
-        acc.push(product.name);
-      }
-      return acc;
-    }, []);
-    
-    card.innerHTML = createMarkup(result, shortNames);
-
- }
-
- 
- 
-async function checkLocalStorage() {
+async function addToBasket() {
   const result = await getDiscountProducts();
-  let cart = JSON.parse(localStorage.getItem("cart")) || [];
-  
-  let productId = result.find(item => item._id)._id;
-  console.log(productId);
-
-  let productIndex = cart.indexOf(productId);
-
-  if(productIndex > -1){
-    button.classList.add('disabled');
-  }else{
-    const button = document.querySelector('.basket');
-    button.addEventListener('click', handleClick);
-    function handleClick() {
-      cart.push(productId);
-      localStorage.setItem("cart", JSON.stringify(cart));
-  
-        button.classList.add('active');
-    button.removeEventListener("click", handleClick);
-  }
+  console.log(result);
+  const shortNames = result.reduce((acc, product) => {
+    if (product.name.length > 10) {
+      acc.push(product.name.slice(0, 10) + "...");
+    } else {
+      acc.push(product.name);
     }
-    
- 
-  
+    return acc;
+  }, []);
+
+  card.innerHTML = createMarkup(result, shortNames);
+
+  // Виклик модалки
+  card.addEventListener('click', (event) => {
+    const item = event.target.closest('.card_item');
+    if (item) {
+      const id = item.dataset.id;
+      openModal(id);
+    }
+  });
+
+  const btn = document.querySelectorAll('.basket');
+  console.log(btn);
+
+  btn.forEach((button) => {
+    button.addEventListener("click", handleAddToCart);
+  });
 }
+
+function handleAddToCart(e) {
+  const button = e.currentTarget;
+  console.log(button);
+  const id = button.dataset.id;
+  console.log(id);
+
+  if (button.hasAttribute("disabled")) {
+   console.log(removeFromStorageCart(id));
+    button.removeAttribute("disabled");
+    button.innerHTML = `
+      <svg class="basket-icon" width="18" height="18">
+        <use href="../img/icons.svg#icon-cart"></use>
+      </svg>
+    `;
+  } else {
+    addToStorageCart(id);
+    console.log(addToStorageCart(button.dataset.id));
+    button.setAttribute("disabled", true);
+    button.innerHTML = `
+      <svg class="basket-icon-check" width="18" height="18">
+        <use href="../img/icons.svg#icon-check"></use>
+      </svg>
+    `;
+  }
+}
+
 addToBasket();
-checkLocalStorage();
+
+ 
+
 
 
 
