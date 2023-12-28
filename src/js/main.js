c2993885285830763f795f2b5e154ba3e5146454
lilshudd <max.fedorak10@gmail.com>
2023-12-28 20:32:57 +0200

    start

--- src/js/main.js
+++ src/js/main.js
@@ -4,4 +4,4 @@ import './pagination.js';
 import './modal_window.js';
 import './footer.js';
 import './discount.js';
-
+import './popular_products.js';
---
+++ src/js/popular_products.js
@@ -0,0 +1,51 @@
+import { getPopularProducts } from './fetchAPI';
+
+const product_list = document.querySelector('.product-card');
+
+function createPopularList() {
+  getPopularProducts()
+    .then(result => {
+      createPopularMarkup(result);
+    })
+    .catch(error => {
+      throw new Error(error);
+    });
+}
+
+function createPopularMarkup(array) {
+  const markup = array
+    .map(({ category, img, name, popularity, size }) => {
+      return `<li class="product-content">
+        <div class="background-img">
+            <img src="${img}" alt="${name}" class="product-image" />
+        </div>
+        <div class="product-txt-bar">
+            <h3 class="product-title">${name}</h3>
+            <p class="product-category">
+                <span class="span-category">Category:</span>${String(
+                  category
+                ).replace(`_`, ` `)}
+            </p>
+            <div class="product-txt-two">
+                <p class="product-size">
+                    <span class="span-category">Size:</span>${size}
+                </p>
+                <p class="product-popularity">
+                    <span class="span-category">Popularity:</span>${popularity}
+                </p>
+            </div>
+        </div>
+        <div class="svg-svg">
+            <svg class="svg-item" width="12" height="12">
+                <use href="./img/icons.svg#icon-cart"></use>
+            </svg>
+        </div>
+    </li>`;
+    })
+    .join('');
+  product_list.insertAdjacentHTML('beforeend', markup);
+}
+
+createPopularList();
+
+export { createPopularList };
