92aaf49a77126a7ed6dd4c6c08f71f7850041fab
KateDro <106846150+KateDro@users.noreply.github.com>
2023-12-28 11:40:17 +0100

    upd

--- src/js/cart.js
+++ src/js/cart.js
@@ -8,17 +8,17 @@ const selectors = {
   total_price: document.querySelector('.total-price'),
 };
 
-function displayCart() {
-  let j = 0;
-  document.getElementById(
-    'card-counter'
-  ).innerHTML = `<img class="basket-img" src = "../img/yellow-basket.png" width = "164px" height = "140px" alt = "Yellow shopping basket" >
-		<h2 class="basket-title">
-			Your basket is <span class="accent-txt">empty...</span>
-		</h2>
-		<p class="basket-txt">Go to the main page to select your favorite products and add them to the cart.</p>
-`;
-}
+// function displayCart() {
+//   let j = 0;
+//   document.getElementById(
+//     'card-counter'
+//   ).innerHTML = `<img class="basket-img" src = "../img/yellow-basket.png" width = "164px" height = "140px" alt = "Yellow shopping basket" >
+// 		<h2 class="basket-title">
+// 			Your basket is <span class="accent-txt">empty...</span>
+// 		</h2>
+// 		<p class="basket-txt">Go to the main page to select your favorite products and add them to the cart.</p>
+// `;
+// }
 
 const cart = JSON.parse(localStorage.getItem('cart')) || [];
 
