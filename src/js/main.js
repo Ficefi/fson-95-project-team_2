84a270330fceee9c94a279166697b951da7bf2ca
Ficefi <139962804+Ficefi@users.noreply.github.com>
2023-12-28 21:18:29 +0200

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,7 @@
-
-
 import './fetchAPI.js';
 import './localStorage.js';
 import './pagination.js';
 import './modal_window.js';
 import './footer.js';
 import './discount.js';
-
+import './popular_products.js'
