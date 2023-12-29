94cb4e3f89efbe0f5bb6b89809e797d37eb3b609
KateDro <106846150+KateDro@users.noreply.github.com>
2023-12-29 11:57:23 +0100

    -hr

--- src/js/cart.js
+++ src/js/cart.js
@@ -6,8 +6,11 @@ const selectors = {
   basket_items: document.querySelector('.basket-items'),
   number_of_products: document.querySelector('.number-of-products'),
   total_price: document.querySelector('.total-price'),
+  delete_all_btn: document.querySelector('.del-all-btn'),
+  order_total: document.querySelector('.order-total'),
+  cart_form_authorization: document.querySelector('.cart-form-authorization'),
+  cart_label_wrap: document.querySelector('.cart-label-wrap')
 };
-
 // function displayCart() {
 //   let j = 0;
 //   document.getElementById(
@@ -22,10 +25,19 @@ const selectors = {
 
 const cart = JSON.parse(localStorage.getItem('cart')) || [];
 
+
 //////////////// IF SOMETHING IN CART HIDE BASKET IMAGE///////////////
 
 if (cart.length > 0) {
   selectors.cart.style.display = 'none';
+
+} else if (cart.length === 0) {
+  selectors.cart.style.display = 'flex';
+  selectors.basket_items.style.display = 'none';
+  selectors.delete_all_btn.style.display = 'none';
+  selectors.order_total.style.display = 'none';
+  selectors.cart_form_authorization.style.display = 'none';
+  selectors.cart_label_wrap.style.display = 'none'
 }
 
 ///////////////NUMBER OF PRODUCTS IN CART///////////
@@ -42,7 +54,7 @@ async function addItem(id) {
   await getProductById(id).then(response => {
     const { name, category, size, price, img, desc, _id } = response;
     itemPrice = price;
-    const markup = `<li class='basket-item' data-id="${_id}">
+    const markup = `<li class='basket-item' data-id="${_id}" data-price="${price}">
   <button class='remove-item' data-modal-close aria-label='close modal button'>
         <svg class='remove-item-image' width='15' height='15'>
           <use href='./img/icons.svg#icon-close-btn'></use>
@@ -63,7 +75,7 @@ async function addItem(id) {
 
      </div>
 
-     </li> <hr>
+     </li>
      `;
 
     selectors.basket_items.insertAdjacentHTML('beforeend', markup);
@@ -75,14 +87,28 @@ async function addAllItem() {
   for (const item of cart) {
     if (!item) {
     } else {
-      await addItem(item).then(price => (totalPrice += price));
+      await addItem(item).then(price => totalPrice += price);
     }
   }
 }
 
-selectors.total_price.textContent = '$' + totalPrice.toFixed(2);
 
-addAllItem();
+
+addAllItem().then(resp => {
+  selectors.total_price.textContent = '$' + totalPrice.toFixed(2);
+  //////////////////  SUBMIT FORM ////////////////
+
+  selectors.cart_form_authorization.addEventListener('submit', submitForm)
+
+  function submitForm() {
+    selectors.cart_form_authorization.reset();
+    deleteAllFromBasket();
+  }
+})
+
+
+
+
 
 selectors.basket_items.addEventListener('click', handleRemove);
 
@@ -92,16 +118,27 @@ function handleRemove(e) {
   if (e.target.className !== 'remove-item') {
     return;
   }
+
   const item = e.target.closest('.basket-item');
+
+  totalPrice = totalPrice - Number(item.dataset.price);
+
+  selectors.total_price.textContent = '$' + totalPrice.toFixed(2);
+
   selectors.basket_items.removeChild(item);
   removeFromStorageCart(item.dataset.id);
 }
 
 ////////////DELETE ALL BUTTON LOGIC///////////
-
+selectors.delete_all_btn.addEventListener('click', deleteAllFromBasket)
 function deleteAllFromBasket() {
   localStorage.removeItem('cart');
-  selectors.cart.style.display = 'block';
+  selectors.cart.style.display = 'flex';
+  selectors.basket_items.style.display = 'none';
+  selectors.delete_all_btn.style.display = 'none';
+  selectors.order_total.style.display = 'none';
+  selectors.cart_form_authorization.style.display = 'none';
+  selectors.cart_label_wrap.style.display = 'none'
 }
 
 // TESTS
