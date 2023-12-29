cb3241bcea5f522dce12fb99c97433a7d6ea2074
KateDro <106846150+KateDro@users.noreply.github.com>
2023-12-29 13:03:09 +0100

    upd

--- src/js/header.js
+++ src/js/header.js
@@ -1,4 +1,5 @@
+document.querySelector('.qty-card-products')
 let qtyCardProducts = cart.length;
 
-const cart = JSON.parse(localStorage.getItem('cart')) || [];
-qty-card-products.textContent = String(qtyCardProducts);
+const cart = JSON.parse(localStorage.getItem('qty-card-products')) || [];
+selector.qty-card-products.textContent = String(qtyCardProducts);
