d5c886ddeabc75969e920940a5ab817e9cd5f373
KateDro <106846150+KateDro@users.noreply.github.com>
2023-12-29 12:38:23 +0100

    +head-js

--- src/js/cart.js
+++ src/js/cart.js
@@ -9,35 +9,22 @@ const selectors = {
   delete_all_btn: document.querySelector('.del-all-btn'),
   order_total: document.querySelector('.order-total'),
   cart_form_authorization: document.querySelector('.cart-form-authorization'),
-  cart_label_wrap: document.querySelector('.cart-label-wrap')
+  cart_label_wrap: document.querySelector('.cart-label-wrap'),
 };
-// function displayCart() {
-//   let j = 0;
-//   document.getElementById(
-//     'card-counter'
-//   ).innerHTML = `<img class="basket-img" src = "../img/yellow-basket.png" width = "164px" height = "140px" alt = "Yellow shopping basket" >
-// 		<h2 class="basket-title">
-// 			Your basket is <span class="accent-txt">empty...</span>
-// 		</h2>
-// 		<p class="basket-txt">Go to the main page to select your favorite products and add them to the cart.</p>
-// `;
-// }
 
 const cart = JSON.parse(localStorage.getItem('cart')) || [];
-
 
 //////////////// IF SOMETHING IN CART HIDE BASKET IMAGE///////////////
 
 if (cart.length > 0) {
   selectors.cart.style.display = 'none';
-
 } else if (cart.length === 0) {
   selectors.cart.style.display = 'flex';
   selectors.basket_items.style.display = 'none';
   selectors.delete_all_btn.style.display = 'none';
   selectors.order_total.style.display = 'none';
   selectors.cart_form_authorization.style.display = 'none';
-  selectors.cart_label_wrap.style.display = 'none'
+  selectors.cart_label_wrap.style.display = 'none';
 }
 
 ///////////////NUMBER OF PRODUCTS IN CART///////////
@@ -87,28 +74,22 @@ async function addAllItem() {
   for (const item of cart) {
     if (!item) {
     } else {
-      await addItem(item).then(price => totalPrice += price);
+      await addItem(item).then(price => (totalPrice += price));
     }
   }
 }
-
-
 
 addAllItem().then(resp => {
   selectors.total_price.textContent = '$' + totalPrice.toFixed(2);
   //////////////////  SUBMIT FORM ////////////////
 
-  selectors.cart_form_authorization.addEventListener('submit', submitForm)
+  selectors.cart_form_authorization.addEventListener('submit', submitForm);
 
   function submitForm() {
     selectors.cart_form_authorization.reset();
     deleteAllFromBasket();
   }
-})
-
-
-
-
+});
 
 selectors.basket_items.addEventListener('click', handleRemove);
 
@@ -127,10 +108,15 @@ function handleRemove(e) {
 
   selectors.basket_items.removeChild(item);
   removeFromStorageCart(item.dataset.id);
+
+  const cart = JSON.parse(localStorage.getItem('cart')) || [];
+  let numberOfProducts = cart.length;
+
+  selectors.number_of_products.textContent = String(numberOfProducts);
 }
 
 ////////////DELETE ALL BUTTON LOGIC///////////
-selectors.delete_all_btn.addEventListener('click', deleteAllFromBasket)
+selectors.delete_all_btn.addEventListener('click', deleteAllFromBasket);
 function deleteAllFromBasket() {
   localStorage.removeItem('cart');
   selectors.cart.style.display = 'flex';
@@ -138,7 +124,7 @@ function deleteAllFromBasket() {
   selectors.delete_all_btn.style.display = 'none';
   selectors.order_total.style.display = 'none';
   selectors.cart_form_authorization.style.display = 'none';
-  selectors.cart_label_wrap.style.display = 'none'
+  selectors.cart_label_wrap.style.display = 'none';
 }
 
 // TESTS
